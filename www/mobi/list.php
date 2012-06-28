464137da5757c660538079e137564f11190274e9
Cronix <h@xx0r.eu>
2012-06-28 11:32:55 +0200

    list.php initial working commit, outputs json which is parseable by the miui theme manager app

--- www/mobi/list.php
+++ www/mobi/list.php
@@ -4,18 +4,53 @@ $sortby		=	$_GET["sortby"];
 $start		=	$_GET["start"];
 $count		=	$_GET["count"];
 
+$hostname = 'localhost';
+$username = 'root';
+$password = 'pass';
+$dbname   = 'miui_market';
+
+header("Content-Type: application/json; charset=utf-8");
+
+
+/*** create a new mysqli object with default database***/
+$mysqli = @new mysqli($hostname, $username, $password, $dbname);
+
+/* check connection */ 
+if(!mysqli_connect_errno())
+    {
+    /*** our SELECT query ***/
+	$sql = "SELECT *
+        	FROM list
+        	WHERE moduleType LIKE '".$category."'
+        	ORDER BY 'id'
+        	LIMIT ".$start.", ".$count."
+        	";
+
+    /*** prepare statement ***/
+    if($stmt = $mysqli->prepare($sql))
+        {
+        /*** execute our SQL query ***/
+        $stmt->execute();
+        /*** bind the results ***/
+        $stmt->bind_result($id, $name, $moduleId, $fileSize, $moduleType, $assemblyId, $frontCover, $playTime);
+	$json_foo = '{"Compound":[';
+        /*** loop over the result set ***/
+        while ($stmt->fetch())
+            {
+            /*** echo our table rows ***/
+            $json_foo.='{"assemblyId":"'.$assemblyId.'","downloadUrlRoot":"http://market.s0uthp4rk.eu/mobi/thumbnail/","fileSize":'.$fileSize.',"frontCover":"'.$frontCover.'","moduleId":"'.$moduleId.'","moduleType":"'.$moduleType.'","name":"'.$name.'","playTime":'.$playTime.'},';
+            }
+        }
+    /*** close connection ***/
+    $mysqli->close();
+    }
+else
+    {
+    /*** if we are unable to connect ***/
+    echo 'Unable to connect';
+    exit();
+    }
+$json_foo.= "]}";
+$cleaned_json = str_replace("},]}","}]}", $json_foo);
+echo $cleaned_json;
 ?>
-
-
-{
-	"Compound":[{
-		"assemblyId":"d0d368cd-ac4c-4304-b30d-80ce5a15bee1",
-		"downloadUrlRoot":"http://market.s0uthp4rk.eu/thumbnail/",
-		"fileSize":20548493,
-		"frontCover":"e18/a64fac1e6868f96ab1d5ccfeec7e5f90394e5821",
-		"moduleId":"8f3dd75e-4a69-44fc-95c9-2763c0d8d74a",
-		"moduleType":"compound",
-		"name":"Glaciens V4",
-		"playTime":0
-	}]
-}
