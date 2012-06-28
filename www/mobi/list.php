a05b6a6aca765a9fda8f8f6e39bccb7b83d199d5
Cronix <h@xx0r.eu>
2012-06-28 08:50:24 +0200

    Added initial www folder stuff

---
+++ www/mobi/list.php
@@ -0,0 +1,21 @@
+<?php
+$category	=	$_GET["category"];
+$sortby		=	$_GET["sortby"];
+$start		=	$_GET["start"];
+$count		=	$_GET["count"];
+
+?>
+
+
+{
+	"Compound":[{
+		"assemblyId":"d0d368cd-ac4c-4304-b30d-80ce5a15bee1",
+		"downloadUrlRoot":"http://market.s0uthp4rk.eu/thumbnail/",
+		"fileSize":20548493,
+		"frontCover":"e18/a64fac1e6868f96ab1d5ccfeec7e5f90394e5821",
+		"moduleId":"8f3dd75e-4a69-44fc-95c9-2763c0d8d74a",
+		"moduleType":"compound",
+		"name":"Glaciens V4",
+		"playTime":0
+	}]
+}
