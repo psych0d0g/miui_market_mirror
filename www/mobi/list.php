9d9daae3b495f2e207652e1a5198158623bc1418
Cronix <h@xx0r.eu>
2012-07-03 17:35:56 +0200

    Added GPL License notification

--- www/mobi/list.php
+++ www/mobi/list.php
@@ -1,4 +1,24 @@
 <?php
+
+/*
+#    This script gets the items from mysql db and prints them to the client in json syntax
+#    Copyright (C) 2012  Lukas W.
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU General Public License as published by
+#    the Free Software Foundation version 2 of the License.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU General Public License for more details.
+#
+#    You should have received a copy of the GNU General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+
+
 $category	=	$_GET["category"];
 $sortby		=	$_GET["sortby"];
 $start		=	$_GET["start"];
