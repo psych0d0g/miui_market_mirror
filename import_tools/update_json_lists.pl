260d7d41d6b5fe0e742958f0bea5395f04394b27
Cronix <h@xx0r.eu>
2012-07-15 15:05:00 +0200

    added importing of all images from detail view to the json update script

--- import_tools/update_json_lists.pl
+++ import_tools/update_json_lists.pl
@@ -24,6 +24,7 @@ use Config::Tiny;
 use Data::Dumper;
 use File::Basename;
 
+# Initialization of some basic stuff
 my $ua = LWP::UserAgent->new;
 
 my $dirname = dirname(__FILE__);
@@ -45,6 +46,12 @@ my @categorys = (
         "RingtoneUnion",
 );
 
+my $itemNum = 0;
+
+##################################################
+# Actually Configurable stuff goes after this line
+##################################################
+
 # Set the start ID for UserAgent handler to grab from xiaomi servers
 # for debugging you can set this pretty high
 my $i = 0;
@@ -52,12 +59,13 @@ my $i = 0;
 # How many items to get each request (50 is hardlimit by xiaomi server)
 my $count = 50;
 
-# Variable used for item numbering
-my $itemNum = 0;
-
 # Setup some values for UserAgent
 # Timeout reasonably high for the crappy China Uplink.
 $ua->timeout(30);
+
+###################################################
+# Dont touch anything beyond this line unless you know what i was and you are about to be doing ;)
+###################################################
 
 # Use a system wide Proxy-Server if configured
 $ua->env_proxy;
@@ -88,7 +96,7 @@ foreach my $category ( @categorys ) {
                                 my $dubl_check = $sth->fetchrow_hashref();
 				# if we already have this entry in our DB, do nothing but notify the user about it
                                 if ( defined $dubl_check->{frontCover} ) {
-                                        print "[ITEM-LIST][". $itemNum . "][". $item->{assemblyId} ."] Item Already imported into database, skipping...\n";
+                                        print "[ITEM-LIST][". $itemNum . "][". $item->{moduleId} ."] Item Already imported into database, skipping...\n";
 				# else import the item with all important values to our items table and tell the user about this aswell
                                 } else {
                                         $dbh->do("INSERT INTO list (name, moduleId, fileSize, moduleType, assemblyId, frontCover, playTime)
@@ -99,10 +107,26 @@ foreach my $category ( @categorys ) {
                                 }
 
 				# check for details and import them aswell but into a diffrent table [WORK IN PROGRESS]
-                                my $responseImage = $ua->get("http://market.xiaomi.com/thm/details/$item->{assemblyId}");
+                                my $responseImage = $ua->get("http://market.xiaomi.com/thm/details/$item->{moduleId}");
                                 if ($responseImage->is_success) {
                                         my $jsonImage = decode_json $responseImage->decoded_content;
-                                        print Dumper( $jsonImage );
+					my $mysqlRefId = $jsonImage->{"moduleId"};
+                                	for my $image( @{$jsonImage->{"snapshotsUrl"}} ){
+						my $sth2 = $dbh->prepare("SELECT moduleId,snapshotsUrl FROM images WHERE moduleId='$mysqlRefId' AND snapshotsUrl='$image'");
+						$sth2->execute();
+						my $dubl_check_Images = $sth2->fetchrow_hashref();
+						# if we already have this entry in our DB, do nothing but notify the user about it
+                        		        if ( defined $dubl_check_Images->{"snapshotsUrl"} ) {
+                                		        print "[ITEM-IMAGE][". $itemNum . "][". $image ."] Image Already imported into database, skipping...\n";
+                                		# else import the item with all important values to our items table and tell the user about this aswell
+                                		} else {
+							$dbh->do("INSERT INTO images (moduleId, snapshotsUrl) VALUES('$mysqlRefId','$image')");
+		                                        print "[ITEM-IMAGE][" . $itemNum . "]";
+                		                        print $image . " Imported for Item ";
+                        		                print $mysqlRefId . "\n";
+						}
+
+					}
                                 }
 
                         };
